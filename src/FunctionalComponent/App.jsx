0181042a09cc7be1de58d0a172b1d9806ffc77d1
A9856 <adilkhanofficial74@gmail.com>
2025-06-23 08:27:20 +0530

    App.jsx

--- src/FunctionalComponent/App.jsx
+++ src/FunctionalComponent/App.jsx
@@ -14,7 +14,8 @@ export default function App() {
   }
   return (
     <>
-      <BrowserRouter basename={process.env.NODE_ENV === "production" ? "/IndiaNews/" :'/'}>
+      {/* <BrowserRouter basename={process.env.NODE_ENV === "production" ? "/IndiaNews/" :'/'}> */}
+       <BrowserRouter basename="/IndiaNews/">
         <Navbar getLanguage={getLanguage} getSearch={getSearch} />
         <Routes>
           <Route path='' element={<Home language={language} q={search ? search : "All"} />} />
